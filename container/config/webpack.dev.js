693864362f70210a1f19a2846d92102b4997ff22
Salvation_sub <kyman19940214@gmail.com>
2022-11-28 11:31:42 +0900

    production config added for CI/CD

--- container/config/webpack.dev.js
+++ container/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require("webpack-merge");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
@@ -20,9 +19,6 @@ const devConfig = {
       },
       // 프로젝트간의 공통 모듈 공유
       shared: packageJson.dependencies,
-    }),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
     }),
   ],
 };
