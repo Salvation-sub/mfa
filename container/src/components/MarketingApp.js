0158c240511193c93ed6ffea329ffcb7da84d12f
Salvation_sub <kyman19940214@gmail.com>
2022-11-29 10:06:55 +0900

    routing finished

--- container/src/components/MarketingApp.js
+++ container/src/components/MarketingApp.js
@@ -4,18 +4,23 @@ import { useHistory } from "react-router-dom";
 
 export default () => {
   const ref = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
-    mount(ref.current, {
+    const { onParentNavigate } = mount(ref.current, {
+      // marketing app 안에서 일어나는 routing 처리
       onNavigate: ({ pathname: nextPathname }) => {
         const { pathname } = history.location;
 
         if (pathname !== nextPathname) {
-          history.pushState(nextPathname);
+          history.push(nextPathname);
         }
       },
     });
+
+    // container에서 일어나는 routing 처리
+    history.listen(onParentNavigate);
   }, []);
 
-  return <div ref={ref}></div>;
+  return <div ref={ref} />;
 };
--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,26 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createMemoryHistory } from "history";
+import { createMemoryHistory, createBrowserHistory } from "history";
 import App from "./App";
 
 // Mount function to start up the app
-const mount = (el, { onNavigate }) => {
-  const history = createMemoryHistory();
+const mount = (el, { onNavigate, defaultHistory }) => {
+  // container 일 경우는 메모리 히스토리 , 그게 아닐시 browserhistory를 사용한다.
+  const history = defaultHistory || createMemoryHistory();
 
-  history.listen(onNavigate);
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
 
   ReactDOM.render(<App history={history} />, el);
+
+  return {
+    // container의 history를 가져와 이벤트 처리를 해준다.
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
 };
-// If we are in development and in isolation,
-// call mount immediately
+
+// 개발 환경일 경우 아래를 실행한다.
 if (process.env.NODE_ENV === "development") {
   const devRoot = document.querySelector("#_marketing-dev-root");
 
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
-// We are running through container
-// and we should export the mount function
 export { mount };
