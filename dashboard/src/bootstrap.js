8dd44761fa7060f41723250cc15b7f47a2fcadfa
Salvation_sub <kyman19940214@gmail.com>
2022-11-30 09:11:15 +0900

    added dashboard

---
+++ dashboard/src/bootstrap.js
@@ -0,0 +1,18 @@
+import { createApp } from "vue";
+import Dashboard from "./components/Dashboard.vue";
+
+const mount = (el) => {
+  const app = createApp(Dashboard);
+  app.mount(el);
+};
+
+// 개발 환경일 경우 아래를 실행한다.
+if (process.env.NODE_ENV === "development") {
+  const devRoot = document.querySelector("#_dashboard-dev-root");
+
+  if (devRoot) {
+    mount(devRoot);
+  }
+}
+
+export { mount };
